947dc968af0e1447d917d81152262e418600a4bd
Sandeep Verma <SandeepVerma@shoebs-mac-mini.local>
2018-05-02 16:36:04 +0530

    Initial Commit

---
+++ circularImageInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  circularImageInSwift
+//
+//  Created by Sandeep Verma on 5/2/18.
+//  Copyright © 2018 Loginworks Softwares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
